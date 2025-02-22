8f4d6fd68ba5f1ae6e2ccdaf7d104b9f40813f2c
virag-ky <vkormoczy@gmail.com>
2025-02-22 13:34:55 +0200

    Add navbar, icons with default styles

--- src/renderer/src/components/Navbar.jsx
+++ src/renderer/src/components/Navbar.jsx
@@ -1,5 +1,20 @@
+import { TiHome } from 'react-icons/ti'
+import { FaUser } from 'react-icons/fa'
+import { IoMdSettings } from 'react-icons/io'
+import { FaPlus } from 'react-icons/fa6'
+
 function Navbar() {
-  return <nav></nav>
+  return (
+    <nav>
+      <div className="main-navigation-icons">
+        <TiHome className="nav-icon" />
+        <FaUser className="nav-icon" />
+        <FaPlus className="nav-icon" />
+      </div>
+
+      <IoMdSettings className="nav-icon" />
+    </nav>
+  )
 }
 
 export default Navbar
