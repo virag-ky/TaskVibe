982ccacc54ebbae9be6ed2d33b329535446b1d2d
virag-ky <vkormoczy@gmail.com>
2025-02-25 13:48:00 +0200

    Display static quote

--- src/renderer/src/components/DailyMotivaton.jsx
+++ src/renderer/src/components/DailyMotivaton.jsx
@@ -2,6 +2,7 @@ function DailyMotivation() {
   return (
     <section>
       <h2>Daily Motivation</h2>
+      <blockquote>The only way to do great work is to love what you do. - Steve Jobs</blockquote>
     </section>
   )
 }
