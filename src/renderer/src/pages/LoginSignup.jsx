05b9dcc0c45199e6c84886ed320d38fbba19fd7f
virag-ky <vkormoczy@gmail.com>
2025-02-27 18:05:42 +0200

    Make Login/Signup inputs controlled

--- src/renderer/src/pages/LoginSignup.jsx
+++ src/renderer/src/pages/LoginSignup.jsx
@@ -1,10 +1,52 @@
-import Login from '../components/Login'
-import Signup from '../components/Signup'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { TextInput } from '@mantine/core'
+import { PasswordInput } from '@mantine/core'
+import { Title } from '@mantine/core'
+import { Text } from '@mantine/core'
+import { CiAt } from 'react-icons/ci'
+import supabase from '../supabase'
 
 function LoginSignup() {
+  const [hasUser, setHasUser] = useState(false)
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const atIcon = <CiAt />
+
+  console.log(username, email, password)
+
   return (
     <section>
-      <Login />
+      <Title order={1}>{hasUser ? 'Login' : 'Signup'}</Title>
+      <form>
+        <TextInput
+          label="Username"
+          withAsterisk
+          placeholder="Enter your username"
+          value={username}
+          onChange={(e) => setUsername(e.currentTarget.value)}
+        />
+        <TextInput
+          withAsterisk
+          rightSectionPointerEvents="none"
+          rightSection={atIcon}
+          label="Email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.currentTarget.value)}
+        />
+        <PasswordInput
+          label="Password"
+          withAsterisk
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.currentTarget.value)}
+        />
+        <button>{hasUser ? 'Login' : 'Create account'}</button>
+      </form>
+      <Text>Already have an account?</Text>
     </section>
   )
 }
