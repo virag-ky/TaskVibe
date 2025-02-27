975b410e373b9934bf69f4a93b6bc6c68e8dfa17
virag-ky <vkormoczy@gmail.com>
2025-02-27 13:36:42 +0200

    Add NumberInput component for the energy level

--- src/renderer/src/components/Greeting.jsx
+++ src/renderer/src/components/Greeting.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react'
+import { NumberInput } from '@mantine/core'
+
 function Greeting() {
+  const [energyLevel, setEnergyLevel] = useState(10)
+
   return (
     <section className="greeting">
       <h1>Hello Virag!</h1>
       <form>
-        <label htmlFor="energy-level">What is your energy level?</label>
-        <input type="number" name="energy-level" id="energy-level" min={1} max={10} />
+        <NumberInput
+          label="What is your energy level?"
+          min={1}
+          max={10}
+          clampBehavior="strict"
+          defaultValue={energyLevel}
+          value={energyLevel}
+          onChange={setEnergyLevel}
+        />
       </form>
     </section>
   )
